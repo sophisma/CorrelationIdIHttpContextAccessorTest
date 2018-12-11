eded076b92caf306cca3ac7a0c68ae1f289de5d9
André Silva <sophisma@gmail.com>
2018-12-11 15:14:41 +0000

    Add project files.

---
+++ ApplicationServices/ISomeService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationServices
+{
+    public interface ISomeService
+    {
+        IEnumerable<string> SomeMethod();
+    }
+}
---
+++ ApplicationServices/SomeService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ApplicationServices
+{
+    public class SomeService : ISomeService
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public SomeService(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public IEnumerable<string> SomeMethod()
+        {
+            var headersValues = httpContextAccessor.HttpContext.Request.Headers.Values.Select(e => e.ToString());
+
+            return headersValues;
+        }
+    }
+}
